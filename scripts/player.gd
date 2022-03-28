70b8ed086abc61eef5b41c9d5022a46ea44eba8d
Fazil Babu <fazil47babu@gmail.com>
2022-03-28 17:42:02 +0530

    Added basic mouse look controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,18 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@export var look_speed: Vector2 = Vector2(0.01, 0.01)
+@export var run_speed_multiplier: float = 1.8
+
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var camera_pivot: Node3D
+var camera: Camera3D
 
+func _ready():
+	camera_pivot = get_node("CameraPivot")
+	camera = get_node("CameraPivot/Camera3D")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -27,4 +36,14 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	if Input.is_action_pressed("run"):
+		velocity *= Vector3(run_speed_multiplier, 1, run_speed_multiplier)
+
 	move_and_slide()
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		if event.relative.x != 0:
+			rotate_object_local(Vector3.UP, event.relative.x * look_speed.x)
+
